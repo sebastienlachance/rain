7cf6bc0ed37593aa12865198f5229bea101f5521
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-16 12:58:21 -0400

    Rendering the Screen pixels array

--- src/com/creatiom/rain/graphics/Screen.java
+++ src/com/creatiom/rain/graphics/Screen.java
@@ -3,13 +3,21 @@ package com.creatiom.rain.graphics;
 public class Screen {
 	
 	private int width, height;
-	private int[] pixels;
+	public int[] pixels;
 	
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		
 		pixels = new int[width * height];
+	}
+	
+	public void render() {
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				pixels[x + y * width] = 0xff00ff;
+			}
+		}
 	}
 	
 }
