d3d0c961bf9ff3230f405e23fe5935854fd05aa3
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-17 07:54:43 -0400

    Clearing the screen

--- src/com/creatiom/rain/graphics/Screen.java
+++ src/com/creatiom/rain/graphics/Screen.java
@@ -12,6 +12,12 @@ public class Screen {
 		pixels = new int[width * height];
 	}
 	
+	public void clear() {
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+	
 	public void render() {
 		for(int y = 0; y < height; y++) {
 			for(int x = 0; x < width; x++) {
