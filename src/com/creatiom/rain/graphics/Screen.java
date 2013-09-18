c780bf5f370611db0abb3ff4a4a6fe3b8d0f72d6
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-18 13:00:57 -0400

    Introduced the SpriteSheet class

--- src/com/creatiom/rain/graphics/Screen.java
+++ src/com/creatiom/rain/graphics/Screen.java
@@ -6,7 +6,7 @@ public class Screen {
 	
 	private int width, height;
 	public int[] pixels;
-	public final int MAP_SIZE = 8;
+	public final int MAP_SIZE = 64;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
 	
 	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
