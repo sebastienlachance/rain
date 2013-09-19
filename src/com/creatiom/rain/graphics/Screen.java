f08eda03d39c8ce4cb8eef9d5a62dfefd2a73d25
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-19 12:34:17 -0400

    Creating the SpriteSheet and displaying the grass sprite

--- src/com/creatiom/rain/graphics/Screen.java
+++ src/com/creatiom/rain/graphics/Screen.java
@@ -38,7 +38,7 @@ public class Screen {
 				int xx = x + xOffset;
 				//if (xx < 0 || xx >= width) break;
 				int tileIndex = ((xx >> 4 ) & MAP_SIZE_MASK) + ((yy >> 4) &  MAP_SIZE_MASK) * MAP_SIZE;
-				pixels[x + y * width] = tiles[tileIndex];
+				pixels[x + y * width] = Sprite.grass.pixels[(x&15)+(y&15) * Sprite.grass.SIZE];
 			}
 		}
 	}
