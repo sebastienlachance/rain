5dbf77010b70c7396c2ca856577c2af479e0a83a
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-19 12:44:22 -0400

    Moving logic

--- src/com/creatiom/rain/Game.java
+++ src/com/creatiom/rain/Game.java
@@ -64,6 +64,7 @@ public class Game extends Canvas implements Runnable {
 		double delta = 0;
 		int frames = 0;
 		int updates =0 ;
+		requestFocus();
 		while(running) {
 			long now = System.nanoTime();
 			delta += (now-lastTime) / ns;
