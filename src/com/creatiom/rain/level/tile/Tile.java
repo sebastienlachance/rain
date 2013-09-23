a6ef1e5594b11c46c8684dcadd8d5ab65a528245
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-23 12:18:17 -0400

    Introducing the Tile class

---
+++ src/com/creatiom/rain/level/tile/Tile.java
@@ -0,0 +1,16 @@
+package com.creatiom.rain.level.tile;
+
+import com.creatiom.rain.graphics.Screen;
+import com.creatiom.rain.graphics.Sprite;
+
+public class Tile {
+	public int x, y;
+	public Sprite sprite;
+	
+	public Tile(Sprite sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void render(int x, int y, Screen screen) {
+	}
+}
