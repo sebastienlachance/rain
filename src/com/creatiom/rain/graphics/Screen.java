b5e6a1542239ae5cbe1cc90fd4bcb4510cc87c67
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-24 12:58:43 -0400

    Procedural rendering

--- src/com/creatiom/rain/graphics/Screen.java
+++ src/com/creatiom/rain/graphics/Screen.java
@@ -42,7 +42,8 @@ public class Screen {
 			int ya = y + yp;
 			for (int x = 0; x < tile.sprite.SIZE; x++) {
 				int xa = x + xp;
-				if (xa < 0 || xa >= width || ya < 0 || ya >= width) break;
+				if (xa < -tile.sprite.SIZE || xa >= width || ya < 0 || ya >= height) break;
+				if (xa < 0) xa = 0;
 				pixels[xa + ya * width] = tile.sprite.pixels[x + y * tile.sprite.SIZE];
 			}
 		}
--- src/com/creatiom/rain/level/Level.java
+++ src/com/creatiom/rain/level/Level.java
@@ -38,9 +38,9 @@ public class Level {
 	public void render(int xScroll, int yScroll, Screen screen) {
 		screen.setOffset(xScroll, yScroll);
 		int x0 = xScroll >> 4; 
-		int x1 = (xScroll + screen.width) >> 4;
+		int x1 = (xScroll + screen.width + 16) >> 4;
 		int y0 = yScroll >> 4;
-		int y1 = (yScroll + screen.height) >> 4;
+		int y1 = (yScroll + screen.height + 16) >> 4;
 		
 		for	(int y = y0; y < y1; y++) {
 			for(int x = x0; x < x1; x++) {
@@ -51,7 +51,7 @@ public class Level {
 	
 	public Tile getTile(int x, int y) {
 		if (x < 0 || y < 0 || x >= width || y >= height) return Tile.voidTile;
-		if (tiles[x + y  *width] == 0) return Tile.grass;
+		if (tiles[x + y * width] == 0) return Tile.grass;
 		return Tile.voidTile;
 	}
 
