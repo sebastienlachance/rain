482b0b2222d3e6eeee7515c859259505ee34022b
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-24 12:20:23 -0400

    Taking Offsets into account

--- src/com/creatiom/rain/graphics/Screen.java
+++ src/com/creatiom/rain/graphics/Screen.java
@@ -11,6 +11,8 @@ public class Screen {
 	public final int MAP_SIZE = 64;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
 	
+	public int xOffset, yOffset;
+	
 	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
 	
 	public Random random = new Random();
@@ -45,6 +47,9 @@ public class Screen {
 	}
 	
 	public void renderTile(int xp, int yp, Tile tile) {
+		xp -= xOffset;
+		yp -= yOffset;
+		
 		for (int y = 0; y < tile.sprite.SIZE; y++) {
 			int ya = y + yp;
 			for (int x = 0; x < tile.sprite.SIZE; x++) {
@@ -54,4 +59,10 @@ public class Screen {
 			}
 		}
 	}
+	
+	public void setOffset(int xOffset, int yOffset) {
+		this.xOffset = xOffset;
+		this.yOffset = yOffset;
+	}
+	
 }
--- src/com/creatiom/rain/level/Level.java
+++ src/com/creatiom/rain/level/Level.java
@@ -36,6 +36,7 @@ public class Level {
 	}
 	
 	public void render(int xScroll, int yScroll, Screen screen) {
+		screen.setOffset(xScroll, yScroll);
 		int x0 = xScroll >> 4; 
 		int x1 = (xScroll + screen.width) >> 4;
 		int y0 = yScroll >> 4;
