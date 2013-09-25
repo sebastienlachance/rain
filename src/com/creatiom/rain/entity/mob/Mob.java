7eb5443e4d40bff9d5baa6c064109aebde5b51cd
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-25 12:23:32 -0400

    The Move method

--- src/com/creatiom/rain/entity/mob/Mob.java
+++ src/com/creatiom/rain/entity/mob/Mob.java
@@ -9,7 +9,16 @@ public abstract class Mob extends Entity {
 	protected int dir = 0;
 	protected boolean moving = false;
 	
-	public void move() {
+	public void move(int xa, int ya) {
+		if (xa > 0) dir = 1;
+		if (xa < 0) dir = 3;
+		if (ya > 0) dir = 2;
+		if (ya < 0) dir = 0;
+			
+		if (!collision()) {
+			x += xa;
+			y += ya;
+		}
 	}
 	
 	public void update() {	
