041ecc76275753cb29f43eb8bcc35be46718f4e4
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-25 12:09:42 -0400

    Mob abstract class

---
+++ src/com/creatiom/rain/entity/mob/Mob.java
@@ -0,0 +1,22 @@
+package com.creatiom.rain.entity.mob;
+
+import com.creatiom.rain.entity.Entity;
+import com.creatiom.rain.graphics.Sprite;
+
+public abstract class Mob extends Entity {
+	
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
+	
+	public void move() {
+	}
+	
+	public void update() {	
+	}
+	
+	private boolean collision() {
+		return false;
+	}
+	
+}
