d8f57a2c20ecbfae155f8a93d9ba43186889c8a9
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-25 07:58:03 -0400

    Entity abstract class

---
+++ src/com/creatiom/rain/entity/Entity.java
@@ -0,0 +1,29 @@
+package com.creatiom.rain.entity;
+
+import java.util.Random;
+
+import com.creatiom.rain.graphics.Screen;
+import com.creatiom.rain.level.Level;
+
+public class Entity {
+	
+	public int x, y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+	
+	public void update() {
+	}
+	
+	public void render(Screen screen) {
+	}
+	
+	public void remove() {
+		//Remove from level
+		removed = true;
+	}
+	
+	public boolean isRemoved() {
+		return removed;
+	}
+}
