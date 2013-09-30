2474de9840223d189df8fdd600123f6e17715264
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-30 12:31:50 -0400

    Started working on the Spawn level

--- src/com/creatiom/rain/level/Level.java
+++ src/com/creatiom/rain/level/Level.java
@@ -23,7 +23,7 @@ public class Level {
 		
 	}
 	
-	private void loadLevel(String path) {
+	protected void loadLevel(String path) {
 		
 	}
 	
---
+++ src/com/creatiom/rain/level/SpawnLevel.java
@@ -0,0 +1,35 @@
+package com.creatiom.rain.level;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.creatiom.rain.level.tile.Tile;
+
+public class SpawnLevel extends Level {
+
+	private Tile[] tiles;
+	private int[] levelPixels;
+	
+	public SpawnLevel(String path) {
+		super(path);
+	}
+	
+	protected void loadLevel(String path) {
+		try {
+			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, levelPixels, 0, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Exception! Could not load level file!");
+		} 
+	}
+	
+	protected void generateLevel() {
+	
+	}
+	
+}
