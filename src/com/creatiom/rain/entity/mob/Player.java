f5108463446207516d57de04d694c0bed05dc03e
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-09-30 12:05:24 -0400

    Moving animation

--- src/com/creatiom/rain/entity/mob/Player.java
+++ src/com/creatiom/rain/entity/mob/Player.java
@@ -8,6 +8,8 @@ public class Player extends Mob {
 
 	private Keyboard input;
 	private Sprite sprite;
+	private int anim = 0;
+	private boolean walking = false;
 	
 	public Player(Keyboard input) {
 		this.input = input;
@@ -23,22 +25,72 @@ public class Player extends Mob {
 	
 	public void update() {
 		int xa = 0, ya = 0;
+		if (anim < 7500) anim++;
+		else anim = 0;
 		if (input.up) ya--;
 		if (input.down) ya++;
 		if (input.left) xa--;
 		if (input.right) xa++;
 		
-		if (xa != 0 || ya != 0) move(xa, ya);
+		if (xa != 0 || ya != 0) {
+			move(xa, ya); 
+			walking = true;
+		} 
+		else { 
+			walking = false;
+		}
 	}
 	
 	public void render(Screen screen) {
 		int flip = 0;
-		if (dir == 0) sprite = Sprite.player_forward;
-		if (dir == 1) sprite = Sprite.player_side;
-		if (dir == 2) sprite = Sprite.player_back;
+		if (dir == 0) { 
+			sprite = Sprite.player_forward;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_forward_1;
+				}
+				else
+				{
+					sprite = Sprite.player_forward_2;
+				}
+			}
+		}
+		if (dir == 1) { 
+			sprite = Sprite.player_side ;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_side_1;
+				}
+				else
+				{
+					sprite = Sprite.player_side_2;
+				}
+			}
+		}
+		if (dir == 2) {
+			sprite = Sprite.player_back;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_back_1;
+				}
+				else
+				{
+					sprite = Sprite.player_back_2;
+				}
+			}
+		}
 		if (dir == 3) {
 			flip = 1;
 			sprite = Sprite.player_side;
+			if (walking) {
+				if (anim % 20 > 10) {
+					sprite = Sprite.player_side_1;
+				}
+				else
+				{
+					sprite = Sprite.player_side_2;
+				}
+			}
 		}
 		screen.renderPlayer(x - 16, y - 16, sprite, flip);
 	}
--- src/com/creatiom/rain/graphics/Sprite.java
+++ src/com/creatiom/rain/graphics/Sprite.java
@@ -11,9 +11,17 @@ public class Sprite {
 	public static Sprite voidSprite = new Sprite(16, 0x1B87E0);
 	
 	public static Sprite player_forward = new Sprite(32, 0, 5, SpriteSheet.tiles);
-	public static Sprite player_back = new Sprite(32, 2, 5, SpriteSheet.tiles);
-	public static Sprite player_left = new Sprite(32, 3, 5, SpriteSheet.tiles);
 	public static Sprite player_side = new Sprite(32, 1, 5, SpriteSheet.tiles);
+	public static Sprite player_back = new Sprite(32, 2, 5, SpriteSheet.tiles);
+	
+	public static Sprite player_forward_1 = new Sprite(32, 0, 6, SpriteSheet.tiles);
+	public static Sprite player_forward_2 = new Sprite(32, 0, 7, SpriteSheet.tiles);
+	
+	public static Sprite player_side_1 = new Sprite(32, 1, 6, SpriteSheet.tiles);
+	public static Sprite player_side_2 = new Sprite(32, 1, 7, SpriteSheet.tiles);
+	
+	public static Sprite player_back_1 = new Sprite(32, 2, 6, SpriteSheet.tiles);
+	public static Sprite player_back_2 = new Sprite(32, 2, 7, SpriteSheet.tiles);
 	
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		SIZE = size;
