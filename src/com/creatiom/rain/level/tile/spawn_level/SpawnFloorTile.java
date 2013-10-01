a879f0f46ed50c56242a79af9ec669dcee49eb10
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-10-01 13:02:36 -0400

    Defining all Spawn level tiles

---
+++ src/com/creatiom/rain/level/tile/spawn_level/SpawnFloorTile.java
@@ -0,0 +1,17 @@
+package com.creatiom.rain.level.tile.spawn_level;
+
+import com.creatiom.rain.graphics.Screen;
+import com.creatiom.rain.graphics.Sprite;
+import com.creatiom.rain.level.tile.Tile;
+
+public class SpawnFloorTile extends Tile {
+
+	public SpawnFloorTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	} 
+	
+}
---
+++ src/com/creatiom/rain/level/tile/spawn_level/SpawnWallTile.java
@@ -0,0 +1,20 @@
+package com.creatiom.rain.level.tile.spawn_level;
+
+import com.creatiom.rain.graphics.Screen;
+import com.creatiom.rain.graphics.Sprite;
+import com.creatiom.rain.level.tile.Tile;
+
+public class SpawnWallTile extends Tile {
+
+	public SpawnWallTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	} 
+	
+	public boolean solid() {
+		return true;
+	}
+}
