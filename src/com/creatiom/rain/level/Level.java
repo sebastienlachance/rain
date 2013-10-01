dc4bbf7857ca807c8867a2c4053be725d767dbf6
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-10-01 12:12:50 -0400

    Getting tiles the right way

--- src/com/creatiom/rain/level/Level.java
+++ src/com/creatiom/rain/level/Level.java
@@ -4,9 +4,9 @@ import com.creatiom.rain.graphics.Screen;
 import com.creatiom.rain.level.tile.Tile;
 
 public class Level {
-	protected Tile[] tiles;
 	protected int width, height;
 	protected int[] tilesInt;
+	protected int[] tiles;
 	
 	public Level(int width, int height) {
 		this.width = width;
@@ -42,26 +42,21 @@ public class Level {
 		int x1 = (xScroll + screen.width + 16) >> 4;
 		int y0 = yScroll >> 4;
 		int y1 = (yScroll + screen.height + 16) >> 4;
-		
 		for	(int y = y0; y < y1; y++) {
 			for(int x = x0; x < x1; x++) {
-				//getTile(x, y).render(x, y, screen);
-				if (x + y * 16 < 0 || x + y * 16 >= 256) {
-					Tile.voidTile.render(x, y, screen);
-					continue;
-				}
-				
-				tiles[x + y * 16 ].render(x, y, screen);
-				
+				getTile(x, y).render(x, y, screen);
 			}
 		}
 	}
 	
+	// Grass = 0xFF00FF00
+	// Flower = 0xFFFFFF00
+	// Rock = 0xFF7F7F00
 	public Tile getTile(int x, int y) {
 		if (x < 0 || y < 0 || x >= width || y >= height) return Tile.voidTile;
-		if (tilesInt[x + y * width] == 0) return Tile.grass;
-		if (tilesInt[x + y * width] == 1) return Tile.flower;
-		if (tilesInt[x + y * width] == 2) return Tile.rock;
+		if (tiles[x + y * width] == 0xFF00FF00) return Tile.grass;
+		if (tiles[x + y * width] == 0xFFFFFF00) return Tile.flower;
+		if (tiles[x + y * width] == 0xFF7F7F00) return Tile.rock;
 		return Tile.voidTile;
 	}
 
--- src/com/creatiom/rain/level/SpawnLevel.java
+++ src/com/creatiom/rain/level/SpawnLevel.java
@@ -8,8 +8,6 @@ import javax.imageio.ImageIO;
 import com.creatiom.rain.level.tile.Tile;
 
 public class SpawnLevel extends Level {
-
-	private int[] levelPixels;
 	
 	public SpawnLevel(String path) {
 		super(path);
@@ -20,9 +18,8 @@ public class SpawnLevel extends Level {
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
 			int w = image.getWidth();
 			int h = image.getHeight();
-			tiles = new Tile[w * h];
-			levelPixels = new int[w * h];
-			image.getRGB(0, 0, w, h, levelPixels, 0, w);
+			tiles = new int[w * h];
+			image.getRGB(0, 0, w, h, tiles, 0, w);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Exception! Could not load level file!");
@@ -33,11 +30,6 @@ public class SpawnLevel extends Level {
 	// Flower = 0xFFFF00
 	// Rock = 0x7F7F00
 	protected void generateLevel() {
-		for(int i = 0; i < levelPixels.length; i++) {
-			if (levelPixels[i] == 0xff00ff00) tiles[i] = Tile.grass;
-			if (levelPixels[i] == 0xffffff00) tiles[i] = Tile.flower;
-			if (levelPixels[i] == 0xff7f7f00) tiles[i] = Tile.rock;
-		}
 	}
 	
 }
