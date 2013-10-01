36e17b65d4343b1b71cfcbff5af48f956f8b315a
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-10-01 12:19:00 -0400

    Fixed map generation

--- src/com/creatiom/rain/level/SpawnLevel.java
+++ src/com/creatiom/rain/level/SpawnLevel.java
@@ -16,8 +16,8 @@ public class SpawnLevel extends Level {
 	protected void loadLevel(String path) {
 		try {
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
-			int w = image.getWidth();
-			int h = image.getHeight();
+			int w = width = image.getWidth();
+			int h = height = image.getHeight();
 			tiles = new int[w * h];
 			image.getRGB(0, 0, w, h, tiles, 0, w);
 		} catch (IOException e) {
