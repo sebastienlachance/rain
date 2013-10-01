711cb433540b7a16e98dccd1692d3feb6af76adb
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-10-01 12:23:17 -0400

    Spawn location

--- src/com/creatiom/rain/Game.java
+++ src/com/creatiom/rain/Game.java
@@ -46,7 +46,7 @@ public class Game extends Canvas implements Runnable {
 		frame = new JFrame();
 		key = new Keyboard();
 		level = new SpawnLevel("/textures/level.png");
-		player = new Player(key);
+		player = new Player(6 * 16, 4 * 16, key);
 		
 		addKeyListener(key);
 	}
