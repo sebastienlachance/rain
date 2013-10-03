253febd0b39ba7476afd900966457d7f51b14dbc
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-10-03 12:31:31 -0400

    Perfecting collision detection

--- src/com/creatiom/rain/entity/mob/Mob.java
+++ src/com/creatiom/rain/entity/mob/Mob.java
@@ -32,7 +32,11 @@ public abstract class Mob extends Entity {
 	
 	private boolean collision(int xa, int ya) {
 		boolean solid = false;
-		if (level.getTile((x + xa) / 16, (y + ya) / 16).solid()) solid = true;
+		for(int c = 0; c < 4; c++) {
+			int xt = ((x + xa) + c % 2 * 14 - 8) / 16;
+			int yt = ((y + ya) + c / 2 * 12 + 3) / 16;
+			if (level.getTile(xt, yt).solid()) solid = true;
+		}
 		return solid;
 	}
 	
