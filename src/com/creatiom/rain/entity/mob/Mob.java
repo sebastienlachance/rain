270fede3d9bd9ca5fdba62624928967173439f6a
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-10-03 12:18:21 -0400

    Sliding

--- src/com/creatiom/rain/entity/mob/Mob.java
+++ src/com/creatiom/rain/entity/mob/Mob.java
@@ -10,6 +10,12 @@ public abstract class Mob extends Entity {
 	protected boolean moving = false;
 	
 	public void move(int xa, int ya) {
+		if (xa != 0 && ya != 0) {
+			move(xa, 0);
+			move(0, ya);
+			return;
+		}
+		
 		if (xa > 0) dir = 1;
 		if (xa < 0) dir = 3;
 		if (ya > 0) dir = 2;
@@ -26,7 +32,7 @@ public abstract class Mob extends Entity {
 	
 	private boolean collision(int xa, int ya) {
 		boolean solid = false;
-		if (level.getTile((x + xa)/16, (y + ya)/16).solid()) solid = true;
+		if (level.getTile((x + xa) / 16, (y + ya) / 16).solid()) solid = true;
 		return solid;
 	}
 	
