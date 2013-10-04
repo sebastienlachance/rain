ab73230a258f83a39e605ae29fec029051e7af94
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-10-04 15:13:10 -0400

    Particle physics

--- src/com/creatiom/rain/entity/particle/Particle.java
+++ src/com/creatiom/rain/entity/particle/Particle.java
@@ -14,7 +14,8 @@ public class Particle extends Entity {
 	private int life;
 	private int time = 0;
 	
-	protected double xx, yy, xa, ya;
+	protected double xx, yy, zz;
+	protected double xa, ya, za;
 
 	public Particle(int x, int y, int life) {
 		this.x = x;
@@ -23,30 +24,39 @@ public class Particle extends Entity {
 		this.yy = y;
 		this.life = life + (random.nextInt(20) - 10);
 		sprite = Sprite.particle_normal;
-		this.xa = random.nextGaussian();
+		this.xa = random.nextGaussian() + 1.8;
+		if (this.xa < 0) xa = 0.1;
 		this.ya = random.nextGaussian();
+		this.zz = random.nextFloat() + 2.0;
 	}
 	
 	public void update() {
 		time++;
 		if (time > 7400) time = 0;
 		if (time > life) remove();
-		if (!collision((int)xa, (int)ya)) {
-			this.xx += xa;
-			this.yy += ya;
-		} 
-	
+		za -= 0.1;
+		
+		if (zz < 0) {
+			zz = 0;
+			za *= -0.55;
+			xa *= 0.4;
+			ya *= 0.4;
+		}
+		
+		this.xx += xa;
+		this.yy += ya;
+		this.zz += za;
 	}
 	
 	public void render(Screen screen) {
-		screen.renderSprite((int)xx, (int)yy, this.sprite, true);
+		screen.renderSprite((int)xx - 10, (int)yy - (int)zz, this.sprite, true);
 	}
 	
 	private boolean collision(double xa, double ya) {
 		boolean solid = false;
 		for(int c = 0; c < 4; c++) {
 			double xt = ((xx + xa) + c % 2 * sprite.getWidth()) / 16;
-			double yt = ((yy + ya) + c / 2 * sprite.getHeight()) / 16;
+			double yt = ((yy - (int)zz + ya) + c / 2 * sprite.getHeight()) / 16;
 			if (level.getTile((int)xt, (int)yt).solid()) solid = true;
 		}
 		return solid;
--- src/com/creatiom/rain/entity/projectile/WizardProjectile.java
+++ src/com/creatiom/rain/entity/projectile/WizardProjectile.java
@@ -20,7 +20,7 @@ public class WizardProjectile extends Projectile {
 	}
 	
 	public void update() {
-		if (level.tileCollision(x, y, nx, ny, 13)) {
+		if (level.tileCollision(x, y, nx, ny, 7)) {
 			level.add(new ParticleSpawner((int)x, (int)y, 44, 50, level));
 			remove();
 		}
