e48d0edd57a4a72026026135a7b5c740d7906cc9
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-10-15 12:52:38 -0400

    Mob movement speed

--- src/com/creatiom/rain/entity/mob/Mob.java
+++ src/com/creatiom/rain/entity/mob/Mob.java
@@ -17,7 +17,7 @@ public abstract class Mob extends Entity {
 	
 	protected Direction dir;
 	
-	public void move(int xa, int ya) {
+	public void move(double xa, double ya) {
 		if (xa != 0 && ya != 0) {
 			move(xa, 0);
 			move(0, ya);
@@ -28,11 +28,27 @@ public abstract class Mob extends Entity {
 		if (xa < 0) dir = Direction.LEFT;
 		if (ya > 0) dir = Direction.DOWN;
 		if (ya < 0) dir = Direction.UP;
-			
-		if (!collision(xa, ya)) {
-			x += xa;
-			y += ya;
-		} 
+		
+		for (int x = 0; x < Math.abs(xa); x++) {
+			if (!collision(abs(xa), ya)) {
+				this.x += abs(xa);
+			} 
+		}
+		
+		for (int y = 0; y < Math.abs(ya); y++) {
+			if (!collision(xa, abs(ya))) {
+				this.y += abs(ya);
+			} 
+		}
+
+	}
+	
+	private int abs(double value) {
+		if (value < 0) {
+			return -1;
+		} else { 
+			return 1;
+		}
 	}
 	
 	public abstract void update();
@@ -44,12 +60,16 @@ public abstract class Mob extends Entity {
 		level.add(p);
 	}
 	
-	private boolean collision(int xa, int ya) {
+	private boolean collision(double xa, double ya) {
 		boolean solid = false;
 		for(int c = 0; c < 4; c++) {
-			int xt = ((x + xa) + c % 2 * 14 - 8) / 16;
-			int yt = ((y + ya) + c / 2 * 12 + 3) / 16;
-			if (level.getTile(xt, yt).solid()) solid = true;
+			double xt = ((x + xa) - c % 2 * 16) / 16;
+			double yt = ((y + ya) - c / 2 * 16) / 16;
+			int ix = (int) Math.ceil(xt);
+			int iy = (int) Math.ceil(yt);
+			if (c % 2 == 0) ix = (int)Math.floor(xt);
+			if (c / 2 == 0) iy = (int)Math.floor(yt);
+			if (level.getTile(ix, iy).solid()) solid = true;
 		}
 		return solid;
 	}
